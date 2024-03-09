b1c94b3f11015eb5d82aef137aeae15e3e0a9df7
sadatnazarli <sedat.nazarli@gmail.com>
2024-03-09 17:34:55 +0400

    new task

--- task/Program.cs
+++ task/Program.cs
@@ -7,24 +7,33 @@
 
 //Cavab-> true
 
-int[] first = {211, 9, -6,12,34,56,32,5,19};
-int[] second = {211, 34 , 19};
+//int[] first = {211, 9, -6,12,34,56,32,5,19};
+//int[] second = {211, 34 , 19};
 
-bool result = false;
-for (int i = 0; i < second.Length; i++)
-{
-    for (int j = 0; j < first.Length; j++)
-    {
-        if (second[i] == first[j])
-        {
-            result = true;
-            break;
-        }
-    }
-    if (!result)
-    {
-        break;
-    }
-}
+//bool result = false;
+//for (int i = 0; i < second.Length; i++)
+//{
+//    for (int j = 0; j < first.Length; j++)
+//    {
+//        if (second[i] == first[j])
+//        {
+//            result = true;
+//            break;
+//        }
+//    }
+//    if (!result)
+//    {
+//        break;
+//    }
+//}
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
+
+
+
+
+
+
+//Write C# program to print all unique element in an array
+int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
